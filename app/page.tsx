6f461414ecdca57cd870ca10c0b66beccaf1a715
Tejas Kamble <70898998+tejaskamble99@users.noreply.github.com>
2026-05-27 16:22:00 +0530

    fix home page

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,16 @@
+import FeaturesSection from "@/component/landingpage/FeaturesSection";
 import HeroSection from "@/component/landingpage/heroSection";
 import Navbar from "@/component/landingpage/Navbar";
+import PriceSection from "@/component/landingpage/PricingSection";
 
 export default function Home() {
   return (
-    <div className="flex flex-col flex-1 items-center justify-center bg-zinc-50 font-sans dark:bg-black">
+    <div className="flex flex-col flex-1 items-center justify-center bg-zinc-50 font-sans dark:bg-black overflow-x-hidden">
       <section>
         <Navbar />
         <HeroSection />
+        <FeaturesSection />
+        <PriceSection />
       </section>
     </div>
   );
